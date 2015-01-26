4ffde217ec543498895e8052c3d523db776add04
Abhinav Kumar <abhinavkumar@outlook.com>
2015-01-26 18:02:23 +0530

    Added test table for storing properties of the site in key value pairs

--- juitieee_ppp.sql
+++ juitieee_ppp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2015 at 04:08 AM
--- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- Generation Time: Jan 26, 2015 at 01:31 PM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,15 +27,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `admins` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `firstn` varchar(25) NOT NULL,
   `lastn` varchar(25) NOT NULL,
   `rights` int(11) NOT NULL,
   `contact` varchar(15) NOT NULL,
   `email` varchar(30) NOT NULL,
   `sec_ques` varchar(100) NOT NULL,
-  `sec_answ` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`)
+  `sec_answ` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -45,15 +44,40 @@ CREATE TABLE IF NOT EXISTS `admins` (
 --
 
 CREATE TABLE IF NOT EXISTS `questions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `quesstr` text NOT NULL,
   `quesimg` varchar(100) NOT NULL,
   `options` text NOT NULL,
   `answer` int(11) NOT NULL,
   `cat` varchar(4) NOT NULL COMMENT '4-char question category code',
-  `points` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `points` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`id`, `quesstr`, `quesimg`, `options`, `answer`, `cat`, `points`) VALUES
+(1, '', '1.jpg', 'one~~two~~three~~four', 1, 'PROG', 25);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE IF NOT EXISTS `test` (
+`id` int(11) NOT NULL,
+  `key` varchar(15) NOT NULL,
+  `value` varchar(25) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `key`, `value`) VALUES
+(1, 'is_open', 'no');
 
 -- --------------------------------------------------------
 
@@ -62,22 +86,73 @@ CREATE TABLE IF NOT EXISTS `questions` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+`id` int(11) NOT NULL,
   `roll` int(11) NOT NULL,
   `firstn` varchar(25) NOT NULL,
   `lastn` varchar(25) NOT NULL,
   `dob` date NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `roll`, `firstn`, `lastn`, `dob`, `password`) VALUES
-(5, 121001, 'satyam', 'kapoor', '2015-02-05', 'hello');
+(1, 121225, 'Abhinav', 'Kumar', '1994-04-13', 'panday');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
